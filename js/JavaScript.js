36d4a81ce25062e6bd5f827afe8b8ab656f57b0c
alfisar <37258487+alfisar@users.noreply.github.com>
2018-03-16 11:58:03 +0700

    update gambar card

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,12 +1,19 @@
 
 $(document).ready(function(){
+	var i = Math.floor(Math.random() * 20);
+	// alert(i);
+	$.getJSON("../js/Data.json",function(jd){
+		$("#div"+i).css("background-image","url("+ jd.gambar[0].url +")");
+		$("#div"+i).css("background-size","cover");
+	// });
+	
 	$("#signin").click(function(){
 		var nama = document.getElementById("Email").value;
 		var pass = document.getElementById("pass").value;
 		// alert("hahah");
-		$.getJSON("../js/Data.json",function(jd){
+		// $.getJSON("../js/Data.json",function(jd){	
+			i = jd.Admin.length;
 			var temp = false;
-			var i = jd.Admin.length;
 			var j = 0;
 			while((temp != true) && (j <= i-1 )){
 				if ((nama == jd.Admin[j].email) && (pass == jd.Admin[j].Pass)) {
