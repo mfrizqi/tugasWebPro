d418cb84841c3f2f2058131e17c5c768fd258d3e
alfisar <37258487+alfisar@users.noreply.github.com>
2018-03-17 16:53:38 +0700

    update gambar random

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -3,23 +3,62 @@ $(document).ready(function(){
 	var i = 0;
 	var temp3 = 0;
 	var j = 0;
+	var h = [];
+	var k = 0;
+	var ceek = false;
+	var temp2 = 1;
 	$.getJSON("../js/Data.json",function(jd){
-	for (var temp2 = 1; temp2<=20; temp2++) {
-		if (jd.gambar[temp3].nama == "Door Bumper") {
-			$("#product"+temp2+" a").attr("href","descriptionpage.html");
+		while(temp2 <= 20){
+			ceek = false;
+			k = 0;
+			j = h.length;
+			i = Math.floor(Math.random()*20);
+			// alert(i);
+			while ((ceek == false) && (k<=j-1)) {
+				if (h[k] == i) {
+					ceek = true
+				}
+				k++;
+			}
+			if (ceek!=true) {
+				if (jd.gambar[i].nama == "Door Bumper") {
+					$("#product"+temp2+" a").attr("href","descriptionpage.html");
+				}
+				$("#product"+temp2+" img").attr("src",jd.gambar[i].url);
+				$("#product"+temp2+" div h4").append(jd.gambar[i].nama);
+				$("#product"+temp2+" div a p").append(jd.gambar[i].price);
+				// temp2++;
+				// temp3++;
+				// h.push(i);
+			}
+			if (ceek!=true) {
+				if (jd.gambar[i].nama == "Door Bumper") {
+					$("#productt"+temp2+" a").attr("href","descriptionpage.html");
+				}
+				$("#productt"+temp2+" img").attr("src",jd.gambar[i].url);
+				$("#productt"+temp2+" div h4").append(jd.gambar[i].nama);
+				$("#productt"+temp2+" div a p").append(jd.gambar[i].price);
+				temp2++;
+				temp3++;
+				h.push(i);
+			}
 		}
-		$("#product"+temp2+" img").attr("src",jd.gambar[temp3].url);
-		$("#product"+temp2+" div h4").append(jd.gambar[temp3].nama);
-		$("#product"+temp2+" div a p").append(jd.gambar[temp3].price);
-		temp3++;
-	}
-	temp3 = 12;
-	for (var temp2 = 1; temp2<=4; temp2++) {
-		$("#productt"+temp2+" img").attr("src",jd.gambar[temp3].url);
-		$("#productt"+temp2+" div h4").append(jd.gambar[temp3].nama);
-		$("#productt"+temp2+" div a p").append(jd.gambar[temp3].price);
-		temp3++;
-	}
+	// for (var temp2 = 1; temp2<=20; temp2++) {
+	// 	if (jd.gambar[temp3].nama == "Door Bumper") {
+	// 		$("#product"+temp2+" a").attr("href","descriptionpage.html");
+	// 	}
+	// 	$("#product"+temp2+" img").attr("src",jd.gambar[temp3].url);
+	// 	$("#product"+temp2+" div h4").append(jd.gambar[temp3].nama);
+	// 	$("#product"+temp2+" div a p").append(jd.gambar[temp3].price);
+	// 	temp3++;
+	// }
+	// temp3 = 12;
+	// for (var temp2 = 1; temp2<=4; temp2++) {
+	// 	$("#productt"+temp2+" img").attr("src",jd.gambar[temp3].url);
+	// 	$("#productt"+temp2+" div h4").append(jd.gambar[temp3].nama);
+	// 	$("#productt"+temp2+" div a p").append(jd.gambar[temp3].price);
+	// 	temp3++;
+	// }
 		
 	$("#btnAddToCart").click(function(){
 		window.location.href = "billingpage.html";
